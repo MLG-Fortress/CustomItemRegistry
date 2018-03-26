ed8cac33906b92f50f234707b51a80d4c920d987
RoboMWM <robomwm@users.noreply.github.com>
2018-03-26 01:33:42 -0700

    mirror default formatting of lore

--- src/main/java/com/robomwm/customitemrecipes/CustomItems.java
+++ src/main/java/com/robomwm/customitemrecipes/CustomItems.java
@@ -241,15 +241,15 @@ class CustomItems implements CommandExecutor
         if (!itemMeta.hasDisplayName())
             player.sendMessage(LazyUtil.getClickableSuggestion("[Set display name]", "/citem name ", "Set display name"));
         else
-            player.sendMessage(LazyUtil.getClickableSuggestion(itemMeta.getDisplayName(), "/citem name " + itemMeta.getDisplayName().replaceAll("\u00A7", "&"), "Change display name"));
+            player.sendMessage(LazyUtil.getClickableSuggestion(ChatColor.RESET + itemMeta.getDisplayName(), "/citem name " + itemMeta.getDisplayName().replaceAll("\u00A7", "&"), "Change display name"));
         if (itemMeta.hasLore())
         {
             player.sendMessage("Lore:");
             for (int i = 0; i < itemMeta.getLore().size(); i++)
             {
                 player.sendMessage(LazyUtil.getClickableSuggestion("[+]", "/citem lore insert " + i + " ", "Insert line above"),
-                        LazyUtil.getClickableSuggestion("[-] ", "/citem lore remove " + i, "Remove line"),
-                        LazyUtil.getClickableSuggestion(itemMeta.getLore().get(i), "/citem lore set " + i + " ", "Modify line"));
+                        LazyUtil.getClickableCommand("[-] ", "/citem lore remove " + i, "Remove line"),
+                        LazyUtil.getClickableSuggestion(ChatColor.DARK_PURPLE.toString() + ChatColor.ITALIC + itemMeta.getLore().get(i), "/citem lore set " + i + " " + itemMeta.getLore().get(i).replaceAll("\u00A7", "&"), "Modify line"));
             }
         }
         player.sendMessage(LazyUtil.getClickableSuggestion("[+] Append lore", "/citem lore add ", "Append lore"));

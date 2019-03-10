0888884982c83ba108bbc4f3aae92bbcfb52a3aa
RoboMWM <robomwm@users.noreply.github.com>
2019-03-09 21:01:57 -0800

    invert search logic

--- src/main/java/com/robomwm/customitemrecipes/recipebook/RecipeBookSearchCommand.java
+++ src/main/java/com/robomwm/customitemrecipes/recipebook/RecipeBookSearchCommand.java
@@ -16,7 +16,6 @@ import org.bukkit.plugin.Plugin;
 import pw.valaria.bookutil.BookUtil;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -85,7 +84,7 @@ public class RecipeBookSearchCommand implements CommandExecutor, TabCompleter {
             String name = recipes.peek().getName();
             Recipe recipe = recipes.poll().getRecipe();
             builder.add(name).cmd("/showrecipe " + ((Keyed)recipe).getKey(), false).color(ChatColor.AQUA).add("\n");
-            if (count++ > 12)
+            if (count++ > 10)
             {
                 count = 0;
                 builder.add("\\p");
@@ -166,9 +165,10 @@ class RecipeMatch implements Comparable<RecipeMatch>
         return name;
     }
 
+    //Inverted so higher scores are at head of queue.
     @Override
     public int compareTo(RecipeMatch o)
     {
-        return this.match - o.match;
+        return o.match - this.match;
     }
 }
